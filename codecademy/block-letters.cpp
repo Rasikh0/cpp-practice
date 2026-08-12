3b9cb4237f108757b67eed47f109f872daafe0f3
Fakhriya Rasikh <fakhriarasikh@gmail.com>
2026-08-11 21:44:55 -0400

    Add block letters output in block-letters.cpp

---
+++ codecademy/block-letters.cpp
@@ -0,0 +1,17 @@
+// I can't stop reading Tolstoy.
+
+#include <iostream>
+
+int main() {
+
+  std::cout << "FFFFF  RRRR \n";
+  std::cout << "F      R    R\n";
+  std::cout << "F      R    R\n";
+  std::cout << "FFF    RRRR \n";
+  std::cout << "F      R R  \n";
+  std::cout << "F      R  R \n";
+  std::cout << "F      R   R\n";
+
+
+return 0;
+}
